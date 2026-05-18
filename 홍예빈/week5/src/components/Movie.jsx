9efdcb50e5fd0d77d976feedb2556b832f27e101
binsm24 <binsm24@sookmyung.ac.kr>
2026-05-18 12:56:43 +0900

    Feat: 5주차 과제

---
+++ 홍예빈/week5/src/components/Movie.jsx
@@ -0,0 +1,49 @@
+import { useNavigate } from "react-router-dom";
+import "./Movie.css";
+import heart from "../assets/heart.svg";
+import heartOutline from "../assets/heart-outline.svg";
+
+function Movie({ movie, onToggle }) {
+  const navigate = useNavigate();
+
+  return (
+    <div className="movie-card" onClick={() => navigate(`/movies/${movie.id}`)}>
+      <div className="poster-container">
+        <img
+          className="poster-img"
+          src={movie.posterImgUrl}
+          alt={movie.title}
+        />
+
+        <button
+          className="like-btn"
+          onClick={(e) => {
+            e.stopPropagation();
+            onToggle(movie.id);
+          }}
+        >
+          <img src={movie.isLiked ? heart : heartOutline} />
+        </button>
+      </div>
+
+      <div className="movie-info">
+        <h2>{movie.title}</h2>
+        <p className="subtitle">{movie.subTitle}</p>
+
+        <ul className="genres">
+          {movie.genres.map((genre, index) => (
+            <li key={index}>{genre}</li>
+          ))}
+        </ul>
+
+        <p className="description">
+          {movie.description.length > 100
+            ? movie.description.substring(0, 100) + "..."
+            : movie.description}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Movie;
